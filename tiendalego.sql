11860300f9effe6a5259e0b943e08eefeed923b5
JuanManuelFriasCortes <juanmanuelfriascortes@gmail.com>
2025-12-03 20:08:25 -0600

    ya se agregan productos al carrito y da el total

--- tiendalego.sql
+++ tiendalego.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 04, 2025 at 01:52 AM
+-- Generation Time: Dec 04, 2025 at 02:59 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
